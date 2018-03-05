813c24423f56988ccbb1ca8da93a5d2286c050d8
medlinj <medlinj@rose-hulman.edu>
2018-03-05 11:28:19 -0500

    Turned in!

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and JD Medlin.
 """
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m4e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -27,3 +27,49 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+thing1 = rg.SimpleTurtle('turtle')
+thing1.pen = rg.Pen('yellow',5)
+thing1.speed = 10
+
+radius = 75
+
+for k in range(10):
+    thing1.draw_circle(radius)
+
+    thing1.pen_up()
+    thing1.right(90)
+    thing1.forward(20)
+    thing1.left(45)
+
+    thing1.pen_down()
+
+    radius = radius - 5
+
+thing2 = rg.SimpleTurtle('turtle')
+thing2.pen = rg.Pen('brown',5)
+thing2.speed = 5
+
+size = 20
+
+thing2.backward(35)
+
+for k in range(6):
+    thing2.draw_square(size)
+
+    thing2.pen_up()
+    thing2.right(45)
+    thing2.forward(10)
+    thing2.left(90)
+
+    thing2.pen_down()
+
+    size = size - 3
+
+
+print('Flower Power!!!')
+
+window.close_on_mouse_click()
