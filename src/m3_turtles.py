71d80a5e754d02c55e9681d584a34a7d4772ff6b
medlinj <medlinj@rose-hulman.edu>
2018-03-05 10:57:30 -0500

    Turned in!

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -102,10 +102,23 @@ natasha.backward(50)
 natasha.right(90)
 natasha.forward(50)
 
+natasha.right(90)
+natasha.forward(90)
+natasha.pen = rg.Pen('blue',10)
+natasha.forward(200)
+
+tatiana = rg.SimpleTurtle('classic')
+tatiana.pen = rg.Pen('purple',50)
+tatiana.speed = 20
+
+tatiana.right(45)
+tatiana.forward(250)
+tatiana.left(90)
+tatiana.backward(50)
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more line of your own code to make one of the
 #   existing SimpleTurtles move some more and/or have different
 #   characteristics.
@@ -119,7 +132,7 @@ natasha.forward(50)
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   The above code  CONSTRUCTS  two SimpleTurtle objects
 #   and gives those objects NAMES:
 #       boris    natasha
@@ -146,7 +159,7 @@ natasha.forward(50)
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
